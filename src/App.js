31240d834f9782fa887afadc53ca98591ea79488
Bhartendra Singh Chauhan <65101955+bhartendrachauhan@users.noreply.github.com>
2023-05-12 23:29:37 +0530

    made changes in logout function

--- src/App.js
+++ src/App.js
@@ -9,34 +9,37 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { base_url } from "./utils/base_url";
 
-axios.interceptors.request.use(async (request) => {
-  if (!Cookies.get("token")) {
-    return request;
-  }
-  request.headers["token"] = Cookies.get("token");
-  request.headers["refresh-token"] = Cookies.get("refresh-token");
-  const { exp } = jwt_decode(Cookies.get("token"));
-  const expDate = new Date(parseInt(exp) * 1000);
-  const currentDate = new Date();
-  console.log(currentDate, expDate);
-  if (currentDate > expDate && Cookies.get("status") == "Y") {
-    Cookies.set("status", "N");
-    try {
-      await axios.get(`${base_url}/api/v1/user/refresh`).then((response) => {
-        Cookies.set("token", response.data.token);
-        Cookies.set("status", "Y");
-      });
-      request.headers["token"] = Cookies.get("token");
-      request.headers["refresh-token"] = Cookies.get("refresh-token");
-    } catch (err) {
-      console.log("refreshtoken error", err);
+axios.interceptors.request.use(
+  async (request) => {
+    if (!Cookies.get("token")) {
+      return request;
     }
+    request.headers["token"] = Cookies.get("token");
+    request.headers["refresh-token"] = Cookies.get("refresh-token");
+    const { exp } = jwt_decode(Cookies.get("token"));
+    const expDate = new Date(parseInt(exp) * 1000);
+    const currentDate = new Date();
+    console.log(currentDate, expDate);
+    if (currentDate > expDate && Cookies.get("status") == "Y") {
+      Cookies.set("status", "N");
+      try {
+        await axios.get(`${base_url}/api/v1/user/refresh`).then((response) => {
+          Cookies.set("token", response.data.token);
+          Cookies.set("status", "Y");
+        });
+        request.headers["token"] = Cookies.get("token");
+        request.headers["refresh-token"] = Cookies.get("refresh-token");
+      } catch (err) {
+        console.log("refreshtoken error", err);
+      }
+    }
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
   }
-  return request
-},(error)=>{
-  console.log(error);
-  return Promise.reject(error);
-});
+);
 
 function App() {
   return (
