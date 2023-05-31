dcc1882f6a0e1199419d7205479190ea9c65fa09
Bhartendra Singh Chauhan <65101955+bhartendrachauhan@users.noreply.github.com>
2023-05-31 23:31:31 +0530

    Base URL cahnged

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,3 @@
 // export const base_url = "https://q-ai-backend-ti5j.onrender.com"
- export const base_url = "http://localhost:3000"
+//  export const base_url = "http://localhost:3000"
+ export const base_url = "https://q-ai-backend-main.onrender.com"
