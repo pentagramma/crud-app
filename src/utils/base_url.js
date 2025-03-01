5cff25c84ba919af0d0da8d96ee77b045fe151db
Sarthak Kumar <sarthak@Sarthaks-MacBook-Air-2.local>
2025-03-01 22:52:17 +0530

    changes

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,3 +1,3 @@
 // export const base_url = "https://q-ai-backend-ti5j.onrender.com"
- export const base_url = "http://localhost:5000"
+ export const base_url = "https://crud-app-backend-2-sja8.onrender.com"
 //  export const base_url = "https://q-ai-backend-main.onrender.com"
